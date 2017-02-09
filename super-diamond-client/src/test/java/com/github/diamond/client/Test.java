f17cb2b04ab81844ec2b6ccd597569049c92a720
tengkai.yuan <tengkai.yuan@dianping.com>
2017-02-09 18:50:23 +0800

    客户端test

--- super-diamond-client/src/test/java/com/github/diamond/client/Test.java
+++ super-diamond-client/src/test/java/com/github/diamond/client/Test.java
@@ -6,6 +6,8 @@
 package com.github.diamond.client;
 import javax.naming.ConfigurationException;
 
+import com.github.diamond.client.event.ConfigurationEvent;
+import com.github.diamond.client.event.ConfigurationListener;
 /**
  *
  * @author <a href="mailto:bsli@ustcinfo.com">li.binsong</a>
@@ -18,11 +20,20 @@ public class Test {
 	 * @throws ConfigurationException
 	 */
 	public static void main(String[] args) throws Exception {
-		PropertiesConfiguration config = new PropertiesConfiguration("localhost", 8283, "cloud-service", "development", "jdbc");
-		config.addConfigurationListener(new ConfigurationListenerTest());
-		System.out.println(config.getString("jdbc.url"));
-		
-		System.in.read();
-	}
+		PropertiesConfiguration config = new PropertiesConfiguration("localhost", 8283, "wed-test", "development","hunshasy");
+		config.addConfigurationListener(new ConfigurationListener(){
 
+			@Override
+			public void configurationChanged(ConfigurationEvent event) {
+				//UPDATE test.key0 中国jhgjghkjh
+				System.out.println(event.getType().name() + " " + event.getPropertyName() + " " + event.getPropertyValue());
+			}
+			
+		});
+		System.out.println(config.getString("test.key0"));
+		
+	}
+	
 }
+
+
